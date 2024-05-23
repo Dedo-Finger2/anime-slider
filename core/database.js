e2c4134d231aa6e3d7c14eacc6a57bfb94d96e15
Greg <antonioimportant@gmail.com>
2024-05-23 13:51:02 -0300

    refactor: changes the path to images and songs

--- core/database.js
+++ core/database.js
@@ -3,60 +3,60 @@ export const database = [
         id: 1,
         title: "Demon Slayer",
         description: "Demon Slayer is a story set in early 20th century Taishō-era Japan, where man-eating demons roam the land each night. The story follows 13-year-old Tanjiro Kamado, whose entire family is murdered save for his sister Nezuko, who has turned into a demon. The Demon Slayers are entirely human, but they employ special elemental breathing techniques, known as 'Breathing Styles', which grant them superhuman strength and increased resistance, helping them fight against demons.",
-        logo: "../images/demon-slayer/demon-slayer-logo.png",
-        cover: "../images/demon-slayer/demon-slayer-cover.png",
-        background: "../images/demon-slayer/demon-slayer-background.jpg",
+        logo: "./images/demon-slayer/demon-slayer-logo.png",
+        cover: "./images/demon-slayer/demon-slayer-cover.png",
+        background: "./images/demon-slayer/demon-slayer-background.jpg",
         ageRestriction: 15,
         releaseYear: 2019,
         gender: "Shounen",
-        song: "./../songs/demon-slayer.mp3"
+        song: "./songs/demon-slayer.mp3"
     },
     {
         id: 2,
         title: "Dragon Ball Super",
         description: "Seven years after the defeat of Majin Buu, Earth is at peace, and its people live free from any dangers lurking in the universe. However, this peace is short-lived; a sleeping threat awakens in the dark reaches of the galaxy: Beerus, the ruthless God of Destruction.",
-        logo: "../images/dragon-ball/dragon-ball-logo.png",
-        cover: "../images/dragon-ball/dragon-ball-cover.jpeg",
-        background: "../images/dragon-ball/dragon-ball-background.jpg",
+        logo: "./images/dragon-ball/dragon-ball-logo.png",
+        cover: "./images/dragon-ball/dragon-ball-cover.jpeg",
+        background: "./images/dragon-ball/dragon-ball-background.jpg",
         ageRestriction: 10,
         releaseYear: 2017,
         gender: "Shounen",
-        song: "./../songs/dragon-ball.mp3"
+        song: "./songs/dragon-ball.mp3"
     },
     {
         id: 3,
         title: "Jojo's bizarre adventure",
         description: "JoJo no Kimyou na Bouken: Diamond wa Kudakenai adapts the fourth part of the JoJo no Kimyou na Bouken manga series. It's the year 1999, 11 years after Jotaro Kujo's desperate fight against DIO in Egypt. Jotaro meets with Josuke Higashikata, the illegitimate son of his grandfather Joseph Joestar.",
-        logo: "../images/jojo/jojo-logo.png",
-        cover: "../images/jojo/jojo-cover.png",
-        background: "../images/jojo/jojo-background.jpg",
+        logo: "./images/jojo/jojo-logo.png",
+        cover: "./images/jojo/jojo-cover.png",
+        background: "./images/jojo/jojo-background.jpg",
         ageRestriction: 13,
         releaseYear: 2016,
         gender: "Shounen",
-        song: "./../songs/jojo.mp3"
+        song: "./songs/jojo.mp3"
     },
     {
         id: 4,
         title: "Naruto",
         description: "Naruto is a Japanese anime television series based on Masashi Kishimoto's manga series of the same name. The story follows a boy named Naruto Uzumaki, who has a demon fox sealed within him. He is shunned and hated by both villagers and other ninja, and is labeled a freak and a danger to all around him. He overcomes these challenges through hard work and perseverance, and slowly earns the respect and appreciation of others",
-        logo: "../images/naruto/naruto-logo.png",
-        cover: "../images/naruto/naruto-cover.jpg",
-        background: "../images/naruto/naruto-background.jpg",
+        logo: "./images/naruto/naruto-logo.png",
+        cover: "./images/naruto/naruto-cover.jpg",
+        background: "./images/naruto/naruto-background.jpg",
         ageRestriction: 16,
         releaseYear: 2002,
         gender: "Shounen",
-        song: "./../songs/naruto.mp3"
+        song: "./songs/naruto.mp3"
     },
     {
         id: 5,
         title: "Death Note",
         description: "Death Note is an anime based on the famous Japanese manga written by Tsugumi Ohba and Takeshi Obata. The story follows a high school student who comes across a supernatural notebook called 'Death Note' that has the power to kill anyone whose name is written into it",
-        logo: "../images/death-note/death-note-logo.png",
-        cover: "../images/death-note/death-note-cover.jpeg",
-        background: "../images/death-note/death-note-background.jpg",
+        logo: "./images/death-note/death-note-logo.png",
+        cover: "./images/death-note/death-note-cover.jpeg",
+        background: "./images/death-note/death-note-background.jpg",
         ageRestriction: 14,
         releaseYear: 2006,
         gender: "Shounen",
-        song: "./../songs/death-note.mp3"
+        song: "./songs/death-note.mp3"
     }
 ]
